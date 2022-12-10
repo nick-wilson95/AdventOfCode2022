1b745d0ce7b86a7386c79cc0cee1704537de0055
nick.xue15@gmail.com <nick.xue15@gmail.com>
2022-12-10 21:45:48 +0000

    day 10 part 1

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -2,4 +2,4 @@
 global using AdventOfCode2022.Extensions;
 using AdventOfCode2022.Solutions.Days;
 
-Console.WriteLine(new Day9().Solve());
+Console.WriteLine(new Day10().Solve());
---
+++ AdventOfCode2022/Solutions/Days/Day10.cs
@@ -0,0 +1,31 @@
+﻿using Array = AdventOfCode2022.Utils.Array;
+
+namespace AdventOfCode2022.Solutions.Days;
+
+public class Day10 : Day<List<int>>
+{
+    protected override string InputFileName => "day10";
+    
+    protected override List<int> Parse(IEnumerable<string> input)
+    {
+        var result = new List<int>{1};
+
+        foreach (var line in input)
+        {
+            result.Add(result.Last());
+            if (line == "noop") continue;
+            
+            var lineValue = int.Parse(line.Split(' ').Last());
+            result.Add(result.Last() + lineValue);
+        }
+
+        return result;
+    }
+
+    protected override object Solve(List<int> input)
+    {
+        return Array.Of(i => 20 + 40 * i, 6)
+            .Select(x => x * input[x - 1])
+            .Sum();
+    }
+}
