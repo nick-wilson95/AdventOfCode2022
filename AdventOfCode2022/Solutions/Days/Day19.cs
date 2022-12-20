8028975d3518fe34807f9bdfc4693dae35524827
Nick <nick.xue15@gmail.com>
2022-12-20 00:01:48 +0000

    day 19 part 1

--- AdventOfCode2022/Solutions/Days/Day19.cs
+++ AdventOfCode2022/Solutions/Days/Day19.cs
@@ -24,59 +24,82 @@ public class Day19 : Day<IEnumerable<int[,]>>
         });
 
 
-    protected override object Solve(IEnumerable<int[,]> input) =>
-        input.Select(BestGeodeCount)
-            .Zip(Enumerable.Range(1, input.Count()))
-            .Select(x => x.First * x.Second)
-            .Sum();
+    protected override object Solve(IEnumerable<int[,]> input)
+    {
+        var sum = 0;
 
-    private int BestGeodeCount(int[,] costs)
+        Parallel.ForEach(input.Zip(Enumerable.Range(1, input.Count())), x =>
+        {
+            var geodes = BestGeodeCount(x.First);
+            sum += geodes * x.Second;
+
+            Console.WriteLine($"Blueprint {x.Second} - {geodes} geodes");
+        });
+
+        return sum;
+    }
+
+    private static int BestGeodeCount(int[,] costs)
     {
         var bestGeodeCount = 0;
-        
-        for (var targetOreRobots = 0;; targetOreRobots++)
-        for (var targetClayRobots = 0;; targetClayRobots++)
-        for (var targetObsidianRobots = 0;; targetObsidianRobots++)
+
+        var maxOreCost = new[] { costs[0, 0], costs[1, 0], costs[2, 0], costs[3, 0] }.Max();
+
+        void Recurse(int elapsed, int[] robots, int[] resources)
         {
-            var robots = new[]{ 1, 0, 0, 0 };
-            var resources = new[]{ 0, 0, 0, 0 };
-        
-            for (var i = 0; i < 24; i++)
+            var nextResources = resources.Zip(robots, (x, y) => x + y).ToArray();
+
+            if (elapsed == 23)
             {
-                resources = resources.Zip(robots, (x, y) => x + y).ToArray();
-
-                if (robots[0] < targetOreRobots && resources[0] > costs[0, 0])
-                {
-                    resources[0] -= costs[0, 0];
-                    robots[0]++;
-                    continue;
-                }
-
-                if (robots[1] < targetClayRobots && resources[0] > costs[1, 0])
-                {
-                    resources[0] -= costs[1, 0];
-                    robots[1]++;
-                    continue;
-                }
-
-                if (robots[2] < targetObsidianRobots && resources[0] > costs[2, 0] && resources[1] > costs[2, 1])
-                {
-                    resources[0] -= costs[2, 0];
-                    resources[1] -= costs[2, 1];
-                    robots[2]++;
-                    continue;
-                }
-
-                if (resources[0] > costs[3, 0] && resources[2] > costs[3, 2])
-                {
-                    resources[0] -= costs[3, 0];
-                    resources[2] -= costs[3, 2];
-                    robots[3]++;
-                }
+                if (nextResources[3] > bestGeodeCount) bestGeodeCount = nextResources[3];
+                return;
             }
 
-            if (resources[3] > bestGeodeCount) bestGeodeCount = resources[3];
+            var canBuildOre = resources[0] >= costs[0, 0] && resources[0] < (24 - elapsed) * maxOreCost;
+            var canBuildClay = resources[0] >= costs[1, 0] && resources[1] < (24 - elapsed) * costs[2, 1];
+            var canBuildObsidian = resources[0] >= costs[2, 0] && resources[1] >= costs[2, 1] && resources[2] < (24 - elapsed) * costs[3, 2];
+            var canBuildGeode = resources[0] >= costs[3, 0] && resources[2] >= costs[3, 2];
+
+            if (canBuildOre)
+            {
+                Recurse(
+                    elapsed + 1,
+                    new[] { robots[0] + 1, robots[1], robots[2], robots[3] },
+                    new[] { nextResources[0] - costs[0, 0], nextResources[1], nextResources[2], nextResources[3] }
+                );
+            }
+            if (canBuildClay)
+            {
+                Recurse(
+                    elapsed + 1,
+                    new[] { robots[0], robots[1] + 1, robots[2], robots[3] },
+                    new[] { nextResources[0] - costs[1, 0], nextResources[1], nextResources[2], nextResources[3] }
+                );
+            }
+            if (canBuildObsidian)
+            {
+                Recurse(
+                    elapsed + 1,
+                    new[] { robots[0], robots[1], robots[2] + 1, robots[3] },
+                    new[] { nextResources[0] - costs[2, 0], nextResources[1] - costs[2, 1], nextResources[2], nextResources[3] }
+                );
+            }
+            if (canBuildGeode)
+            {
+                Recurse(
+                    elapsed + 1,
+                    new[] { robots[0], robots[1], robots[2], robots[3] + 1 },
+                    new[] { nextResources[0] - costs[3, 0], nextResources[1], nextResources[2] - costs[3, 2], nextResources[3] }
+                );
+            }
+
+            if (resources[0] <= maxOreCost)
+            {
+                Recurse(elapsed + 1, robots, nextResources);
+            }
         }
+
+        Recurse(0, new[] { 1, 0, 0, 0 }, new[] { 0, 0, 0, 0 });
 
         return bestGeodeCount;
     }
