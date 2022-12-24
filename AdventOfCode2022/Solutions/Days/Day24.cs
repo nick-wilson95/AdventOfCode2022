4bc6e8a030d0be8bbaf2dc05d70410792308eeca
Nick <nick.xue15@gmail.com>
2022-12-24 23:19:12 +0000

    improve day 24

--- AdventOfCode2022/Solutions/Days/Day24.cs
+++ AdventOfCode2022/Solutions/Days/Day24.cs
@@ -2,7 +2,7 @@
 
 namespace AdventOfCode2022.Solutions.Days;
 
-public class Day24 : Day<Blizzard[]>
+public class Day24 : Day<((int x, int y) start, (int x, int y) end, Blizzard[] blizzards)>
 {
     protected override string InputFileName => "day24";
 
@@ -13,120 +13,105 @@ public class Day24 : Day<Blizzard[]>
         public int Y { get; set; }
     }
 
-    protected override Blizzard[] Parse(IEnumerable<string> input)
+    protected override ((int x, int y) start, (int x, int y) end, Blizzard[] blizzards) Parse(IEnumerable<string> input)
     {
         var inputArray = input.ToArray();
 
+        (int x, int y) start = default;
+        (int x, int y) end = default;
         var blizzards = new List<Blizzard>();
 
         for (var y = 0; y < inputArray.Length; y++)
             for (var x = 0; x < inputArray.First().Length; x++)
             {
-                if (inputArray[y][x] is '>' or '<' or 'v' or '^')
+                var character = inputArray[y][x];
+
+                if (y == 0 && character == '.') start = (x - 1, y - 1);
+                if (y == inputArray.Length - 1 && character == '.') end = (x - 1, y - 1);
+
+                if (character is '>' or '<' or 'v' or '^')
                 {
-                    blizzards.Add(new Blizzard { Direction = inputArray[y][x], X = x - 1, Y = y - 1 });
+                    blizzards.Add(new Blizzard { Direction = character, X = x - 1, Y = y - 1 });
                 }
             }
 
-        return blizzards.ToArray();
+        return (start, end, blizzards.ToArray());
     }
 
-    protected override object Solve(Blizzard[] input)
+    protected override object Solve(((int x, int y) start, (int x, int y) end, Blizzard[] blizzards) input)
     {
-        var width = input.Select(b => b.X).Max() + 1;
-        var length = input.Select(b => b.Y).Max() + 1;
+        var (start, end, blizzards) = input;
 
-        var numBlizzards = new int[width, length];
-        foreach (var blizzard in input) numBlizzards[blizzard.X, blizzard.Y]++;
+        var width = blizzards.Select(b => b.X).Max() + 1;
+        var length = blizzards.Select(b => b.Y).Max() + 1;
 
-        var weatherCycleLength = width * length / GCD(width, length);
+        bool InBounds((int x, int y) q) =>
+            q.x >= 0 && q.y >= 0 && q.x < width && q.y < length;
 
-        var visited = new List<(int cycleTime, int x, int y)>();
-
-        ((int x, int y) from, (int x, int y) to)[] journeys =
+        var intensity = new int[width, length];
+        foreach (var blizzard in blizzards)
         {
-            ((0, -1), (width - 1, length - 1)),
-            ((width - 1, length), (0, 0)),
-            ((0, -1), (width - 1, length - 1)),
-        };
+            intensity[blizzard.X, blizzard.Y]++;
+        }
 
-        var journey = 0;
+        var weatherCycleLength = width * length / IntMath.GCD(width, length);
 
-        var time = 0;
+        var visited = new HashSet<(int cycleTime, int x, int y)>();
 
-        var lastPositions = new (int x, int y)[] { journeys[journey].from };
+        var targets = new Queue<(int x, int y)>(new [] { end, start, end });
 
-        while (true)
+        var target = targets.Dequeue();
+
+        var lastPositions = new (int x, int y)[] { start };
+
+        for (var time = 0;; time++)
         {
-            time++;
-
-            if (lastPositions.Any(x => x == journeys[journey].to))
+            if (lastPositions.Any(x => x == target))
             {
-                journey++;
-                if (journey == journeys.Length) return time;
+                lastPositions = new (int x, int y)[] { target };
+
+                if (!targets.TryDequeue(out target)) return time;
 
                 visited.Clear();
-
-                lastPositions = new (int x, int y)[] { journeys[journey].from };
             }
 
-            foreach (var blizzard in input)
+            foreach (var bliz in blizzards)
             {
-                numBlizzards[blizzard.X, blizzard.Y]--;
+                intensity[bliz.X, bliz.Y]--;
 
-                blizzard.X = blizzard.Direction switch
+                (bliz.X, bliz.Y) = bliz.Direction switch
                 {
-                    '>' => (blizzard.X + 1) % width,
-                    '<' => (blizzard.X + width - 1) % width,
-                    _ => blizzard.X
+                    '>' => ((bliz.X + 1) % width, bliz.Y),
+                    '<' => ((bliz.X + width - 1) % width, bliz.Y),
+                    'v' => (bliz.X, (bliz.Y + 1) % length),
+                    '^' => (bliz.X, (bliz.Y + length - 1) % length),
+                    _ => (bliz.X, bliz.Y)
                 };
 
-                blizzard.Y = blizzard.Direction switch
-                {
-                    'v' => (blizzard.Y + 1) % length,
-                    '^' => (blizzard.Y + length - 1) % length,
-                    _ => blizzard.Y
-                };
-
-                numBlizzards[blizzard.X, blizzard.Y]++;
+                intensity[bliz.X, bliz.Y]++;
             }
 
-            var nextPositions = lastPositions.SelectMany(pos =>
+            var nextPositions = lastPositions.SelectMany(p =>
             {
-                var adjacent = new (int x, int y)[] {
-                        (pos.x - 1, pos.y),
-                        (pos.x, pos.y - 1),
-                        (pos.x, pos.y),
-                        (pos.x + 1, pos.y),
-                        (pos.x, pos.y + 1)
-                    };
+                var adjacent = new (int x, int y)[]
+                {
+                    (p.x - 1, p.y),
+                    (p.x, p.y - 1),
+                    (p.x, p.y),
+                    (p.x, p.y + 1),
+                    (p.x + 1, p.y)
+                };
 
-                return adjacent.Where(p => (p.x >= 0 && p.y >= 0 && p.x < width && p.y < length) || p == journeys[journey].from)
-                    .Where(p => p == journeys[journey].from || numBlizzards[p.x, p.y] == 0)
-                    .Where(p => !visited.Contains((time % weatherCycleLength, p.x, p.y)));
-            })
-                .Distinct()
-                .ToArray();
+                return adjacent.Where(q => (InBounds(q) && intensity[q.x, q.y] == 0) || q == start || q == end)
+                    .Where(q => !visited.Contains((time % weatherCycleLength, q.x, q.y)));
+            }).Distinct().ToArray();
 
-            foreach (var pos in nextPositions)
+            foreach (var (x, y) in nextPositions)
             {
-                visited.Add((time % weatherCycleLength, pos.x, pos.y));
+                visited.Add((time % weatherCycleLength, x, y));
             }
 
             lastPositions = nextPositions.ToArray();
         }
-    }
-
-    private static int GCD(int a, int b)
-    {
-        while (a != 0 && b != 0)
-        {
-            if (a > b)
-                a %= b;
-            else
-                b %= a;
-        }
-
-        return a | b;
     }
 }
---
+++ AdventOfCode2022/Utils/IntMath.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2022.Utils;
+
+public static class IntMath
+{
+    public static int GCD(int a, int b)
+    {
+        while (a != 0 && b != 0)
+        {
+            if (a > b)
+                a %= b;
+            else
+                b %= a;
+        }
+
+        return a | b;
+    }
+}
